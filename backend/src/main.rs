c4462cc5c65690f96913754dee09fb1cb14cb05a
Charles Vandevoorde <charles@vandevoorde.me>
2022-08-12 20:01:52 +0200

    bootstrap init command

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,51 @@
 use dotenv::dotenv;
+use clap::{Subcommand, Parser};
 
 use cirrus_backend::App;
 use cirrus_backend::Config;
+
+#[derive(Parser)]
+#[clap(name = "cirrus", version, about)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+
+    /// Specify port exposed
+    #[clap(short, long)]
+    port: Option<u16>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialize cirrus
+    Init{},
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
+    let cli = Cli::parse();
+
     let config = Config {
         host: String::from("127.0.0.1"),
-        port: 8000,
+        port: cli.port.unwrap_or(8000),
         ui_assets_path: String::from("/path"),
     };
 
+    match &cli.command {
+        Some(Commands::Init{}) => init(&config).await,
+        None => run(&config).await,
+    }
+
+}
+
+async fn init(config: &Config) {
+
+}
+
+async fn run(config: &Config) {
     let app = App::new(&config).unwrap();
 
     println!("Listening on {}:{}...", config.host, config.port);
